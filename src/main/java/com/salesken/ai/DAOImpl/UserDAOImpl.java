8d27065dcb49d9cf8acbc84948ae1b4263b3ff4e
anupsalesken <anupsoni@salesken.ai>
2021-06-08 21:17:53 +0530

    udpate user dao

---
+++ src/main/java/com/salesken/ai/DAOImpl/UserDAOImpl.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.salesken.ai.DAOImpl;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.salesken.ai.dao.UserDAO;
+import com.salesken.ai.model.User;
+import com.salesken.ai.utils.MongoDBUtils;
+
+/**
+ * @author salesken
+ *
+ */
+public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public void insertUser(User user) {
+		MongoDatabase db = MongoDBUtils.getInstance().getDatabase();
+		
+		MongoCollection<User> userCollection = db.getCollection("user",
+				User.class);
+		userCollection.insertOne(user);
+	}
+
+}
---
+++ src/main/java/com/salesken/ai/dao/UserDAO.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.salesken.ai.dao;
+
+import com.salesken.ai.model.User;
+
+/**
+ * @author salesken
+ *
+ */
+public interface UserDAO {
+
+	public void insertUser(User user);
+}
